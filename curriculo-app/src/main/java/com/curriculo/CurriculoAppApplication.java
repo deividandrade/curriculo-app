9758eb6841ac0271beda206a6b333ff7db795e7d
deividandrade <deividandrade.br@gmail.com>
2026-03-21 10:39:46 -0300

    initial commit

---
+++ curriculo-app/src/main/java/com/curriculo/CurriculoAppApplication.java
@@ -0,0 +1,12 @@
+package com.curriculo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurriculoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurriculoAppApplication.class, args);
+    }
+}
